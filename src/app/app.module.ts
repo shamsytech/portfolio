72ed73e4080d9132707cd45ab334cd837a5f84a2
Shams Hasan <shamshasan941@gmail.com>
2025-01-04 21:33:02 -0500

    added in components needed + organized folder structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { ProjectComponent } from './home/project/project.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { HeroSectionComponent } from './home/hero-section/hero-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectComponent,
+    NavbarComponent,
+    HeroSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
