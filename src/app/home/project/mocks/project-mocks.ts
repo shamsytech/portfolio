60b6f2559fb841541e1cc53f56418dd9d5f9829f
Shams Hasan <shamshasan941@gmail.com>
2025-02-07 23:56:13 -0500

    completed html for project component, & added in finished mock data for projects 1 & 3. next steps:  create demo for project 2, & edit sizing of imgs for all projects.

--- src/app/home/project/mocks/project-mocks.ts
+++ src/app/home/project/mocks/project-mocks.ts
@@ -2,18 +2,18 @@
 export const projectMocks = [
     {
         title: "Housing101",
-        description: "A modern, responsive housing application built with Angular and TypeScript. Features dynamic house showcases, search functionality, signup functionality, and clean design.",
+        description: "A responsive housing app featuring dynamic showcases, search, and signup. Automated pass/fail badges for Karma/Jasmine tests using GitHub Actions YAML scripts.",
         demoButton: "https://housing-dusky.vercel.app/",
         githubButton: "https://github.com/shamoosa1/housing-project",
-        image: "assets/images/portfolio-preview.png",
-        badges: ["Angular", "TypeScript", "HTML", "SASS", "Karma", "Jasmine", "YAML", "Vercel"]
+        image: "assets/images/housing-project-img.png",
+        badges: ["Angular", "TypeScript","Karma", "Jasmine", "GitHub Actions", "YAML"]
     },
     {
         title: "Automate the POS!",
         description: "A custom, automated POS system for all employees of J’s Restaurant, built using the JavaFX GUI",
         demoButton: "https://taskapp.yourdomain.com",
         githubButton: "https://github.com/RyanTren/SWE3313-Automation-Project",
-        image: "assets/images/taskapp-preview.png",
+        image: "",
         badges: ["Project Management", "Agile", "Java", "Docker", "MySQL", "YAML"
         ]
     },
@@ -22,7 +22,7 @@ export const projectMocks = [
         description: "An interactive space-themed web app featuring NASA's daily captures since 1995.",
         demoButton: "https://shamoosa1.github.io/nasa-project/",
         githubButton: "https://github.com/shamoosa1/nasa-project",
-        image: "assets/images/taskapp-preview.png",
+        image: "assets/images/nasa-project-img.png",
         badges: ["HTML", "CSS", "JavaScript"]
     }
 ]
